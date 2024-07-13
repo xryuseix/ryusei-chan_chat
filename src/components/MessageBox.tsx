80cd19c7befcd2d084b3f1836148bb43b51739bf
xryuseix <51394682+xryuseix@users.noreply.github.com>
2024-07-14 01:14:20 +0900

    feat: random placeholder

--- src/components/MessageBox.tsx
+++ src/components/MessageBox.tsx
@@ -5,12 +5,32 @@ type Props = {
   boxStyle: React.CSSProperties;
 };
 
+const placeholders: { text: string; weight: number }[] = [
+  { text: "何か話しかけてみよう！", weight: 10 },
+  { text: "あなたは誰？", weight: 5 },
+  { text: "りゅうせいちゃんとお話ししよう！", weight: 5 },
+  { text: "お前を消す方法", weight: 5 },
+];
+
+const getRandomPlaceholder = () => {
+  const totalWeight = placeholders.reduce((acc, cur) => acc + cur.weight, 0);
+  const random = Math.random() * totalWeight;
+  let sum = 0;
+  for (const placeholder of placeholders) {
+    sum += placeholder.weight;
+    if (random < sum) {
+      return placeholder.text;
+    }
+  }
+  return placeholders?.[0].text ?? "";
+};
+
 export const MessageBox = ({ boxStyle }: Props) => {
   return (
     <div style={Object.assign({}, boxStyle, styles.messageBox)}>
       <InputGroup>
         <InputGroup.Input
-          placeholder={"Search..."}
+          placeholder={getRandomPlaceholder()}
           type="search"
           withRing={false}
         />
