0e2c8d814ea830b46f512e4d22269fae3bb243b2
xryuseix <51394682+xryuseix@users.noreply.github.com>
2024-07-13 22:12:41 +0900

    feat: message passing module

--- src/components/live2d.tsx
+++ src/components/live2d.tsx
@@ -1,17 +1,52 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
 export default function Live2d() {
-  const styles = {
-    position: "fixed" as "fixed",
-    bottom: "-25%",
-    left: 0,
-    zIndex: 1000,
-    maxWidth: "300px",
-    width: "40%",
-    height: "70%",
-    overflow: "hidden",
-    border: "none",
-    backgroundColor: "transparent",
-  }
-  return <>
-    <iframe src="/live2d.html" style={styles}></iframe>
-  </>;
+  const [iframe, setIframe] = useState<HTMLIFrameElement | null>(null);
+  const channel = new MessageChannel();
+  const port = channel.port1;
+
+  useEffect(() => {
+    const iframe = document.getElementById("live2d");
+    if (iframe && iframe instanceof HTMLIFrameElement) {
+      setIframe(iframe);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (!iframe) return;
+    const SendMsgToDomainB = () => {
+      var sendData = { message: "Hello from parent" };
+      console.log(iframe, iframe?.contentWindow);
+      if (iframe && iframe.contentWindow) {
+        console.log("Sending message to iframe", channel.port1);
+        iframe.contentWindow.postMessage("init", "*", [channel.port2]);
+        port.postMessage(sendData);
+        port.onmessage = (e) => {
+          console.log("Message received from Iframe:", e.data);
+        }
+      }
+    };
+    SendMsgToDomainB();
+  }, [iframe]);
+
+  return (
+    <>
+      <iframe id="live2d" src="/live2d.html" style={styles}></iframe>
+    </>
+  );
 }
+
+const styles = {
+  position: "fixed" as "fixed",
+  bottom: "-25%",
+  left: 0,
+  zIndex: 1000,
+  maxWidth: "300px",
+  width: "40%",
+  height: "70%",
+  overflow: "hidden",
+  border: "none",
+  backgroundColor: "transparent",
+};
