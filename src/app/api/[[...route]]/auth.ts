ee4209540ee2a80ce498f5aa46ec05f892571a4d
xryuseix <51394682+xryuseix@users.noreply.github.com>
2024-07-27 13:28:48 +0900

    fix: remove salt

--- src/app/api/[[...route]]/auth.ts
+++ src/app/api/[[...route]]/auth.ts
@@ -17,16 +17,6 @@ async function authCheck(c: Context): Promise<AuthRes> {
     return { error: "Unauthorized", status: 401 };
   }
 
-  const NEXTAUTH_SECRET = process.env.NEXTAUTH_SECRET;
-  if (!NEXTAUTH_SECRET) {
-    throw new Error("NEXTAUTH_SECRET is not defined");
-  }
-
-  const salt = process.env.NEXTAUTH_SECRET_SALT;
-  if (!salt) {
-    throw new Error("NEXTAUTH_SECRET_SALT is not defined");
-  }
-
   const userId = session?.user?.id;
   if (userId && userId !== process.env.XRYUSEIX_USER_ID) {
     return { error: "Forbidden", status: 403 };
